83b0e390fdca698778282450a3113f57244d7df5
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-29 14:06:51 +0300

    #1

---
+++ Task_22_Local_Maximum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_22_Local_Maximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int[] array = new int [30];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 100);
+                Console.WriteLine(array[i]);
+            }
+
+            Console.WriteLine("\n\nЛокальные максимумы: \n");
+
+            if (array[0] > array[1])
+                Console.WriteLine(array[0]);
+            
+            for (int i = 2; i < array.Length; i++)
+            {
+                if (array[i-1] > (array[i-2]) && array[i-1] > array[i])
+                    Console.WriteLine(array[i-1]);
+            }
+
+            if (array[array.Length-1] > array[array.Length-2])
+                Console.WriteLine(array[array.Length-1]);
+        }
+    }
+}
---
+++ Task_22_Local_Maximum/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Task_22_Local_Maximum")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task_22_Local_Maximum")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("602f38d1-2ce5-44e7-bc84-78fe7a6dd8d1")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
